18e3b99614e5baa9694675073d0b9c50dfea4a30
Fernando Breno <fernandobrenofx@gmail.com>
2020-09-23 10:59:18 -0300

    Completo( commit on 'master')

--- Aula 2.cpp
+++ Aula 2.cpp
@@ -1,4 +1,5 @@
 #include<cstdio>
+#include <limits>
 long long somafor(int n){
     long long soma=0; //2
     for (size_t i = 0; i < n; i++)//2n+2
@@ -18,9 +19,47 @@ long long somaFormula(int n){
     // --- O(1) ---
 }
 
-void somatoria(){
-    int x = 2+5;
+int menorValor(int n,int vet[]){
+    int min = std::numeric_limits<int>::max();//2
+    for (size_t i = 0; i < n; ++i)//2n+2
+    {
+        if (min>vet[i])//2n
+        {
+            min=vet[i];//2n
+        }
+        
+    }
+    return min;//1
+    //6n+3
+    // --- O(n) ---
 }
+
+bool contido(int s, int A[], int n){
+
+    for (size_t i = 0; i < n; ++i)//2n+2
+    {
+        if (s==A[i])//2n
+        {
+            return true;//1
+        }
+    }
+    return false;//1
+    //4n+3
+    // --- O(n) ---
+}
+
+/*
+Questão 4:
+a)O(1)
+b)O(n)
+c)O(n)
+d)O(n²)
+e)O(n!)
+f)O(n*log n)
+g)O(n³)
+h)O(n^5)
+i)O(k^n)
+*/
 
 int main(){
 
