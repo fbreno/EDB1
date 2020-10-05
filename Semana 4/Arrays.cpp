6b7977dadd59777e261661fb7891f32dabfc7577
fernando <fernando@pop-os.localdomain>
2020-10-05 01:40:00 -0300

    sem cmake ainda

--- Semana 4/Arrays.cpp
+++ Semana 4/Arrays.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <random>
+#include <ctime>
 
-void selectionSort(int t,int* &vet)
+void preencherRandom(int *vet, int t){
+    std::mt19937 gerador;
+    gerador.seed(std::time(0));
+    std::uniform_int_distribution<uint32_t> teste(1,50000);
+
+    for (size_t i = 0; i < t; i++)
+    {
+        vet[i]=teste(gerador);
+    }
+}
+
+void selectionSort(int t,int* vet)
 {   
     for (size_t i = 0; i < t; ++i)
     {
@@ -17,9 +30,46 @@ void selectionSort(int t,int* &vet)
     }
 }
 
+void insertionSort(int t,int* vet)
+{   
+    for (size_t i = 1; i < t; ++i)
+    {   
+        int atual = vet[i];
+        int j = i-1;
+        while (j>=0 && atual<vet[j])
+        {
+            vet[j+1]=vet[j];
+            j--;
+        }
+        vet[j+1]=atual;
+    }
+}
+
+void mostrar(int vet[],int t){
+    for (size_t i = 0; i < t; i++)
+    {
+        std::cout<<vet[i]<<std::endl;
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
+    int tam = atoi(argv[1]);
+    int vet[tam];
+    preencherRandom(vet,tam);
+    if (argv[3]=="selection")
+    {   
+        std::cout<<"selection:"<<std::endl;
+        selectionSort(tam,vet);
+        mostrar(vet,tam);
+    }else if (argv[3]=="insertion")
+    {
+        std::cout<<"insertion:"<<std::endl;
+        insertionSort(tam,vet);
+        mostrar(vet,tam);
+    }
+    
     
     return 0;
 }
